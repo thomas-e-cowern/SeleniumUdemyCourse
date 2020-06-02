ac33f42e9461b5b8cd0cda9c314fecd3adaec224
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-02 08:46:39 -0400

    added lesson 1 & 2

---
+++ JavaTutorial/src/introduction/HelloWorld.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This is my first java program in several weeks
+		/*
+		 * 
+		 */
+		
+		System.out.println("Hello World");
+
+	}
+
+}
