1b97b618d206c8528c33b73c362b8e6a18493af6
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-03 15:35:02 -0400

    update adding method

--- JavaTutorial/src/oopconcepts/AddingMethods.java
+++ JavaTutorial/src/oopconcepts/AddingMethods.java
@@ -3,11 +3,19 @@ package oopconcepts;
 public class AddingMethods {
 
 	public static void main(String[] args) {
-		findGrades("John", 90);
-		findGrades("Tom", 80);
+		String grade;
+		String studentName;
+		
+		studentName = "John";
+		grade = findGrades(90);
+		displayGrades(studentName, grade);
+		
+		studentName = "Tom";
+		grade = findGrades(80);
+		displayGrades(studentName, grade);
 	}
 	
-	public static void findGrades(String studentName, int score) {
+	public static String findGrades(int score) {
 		String grade;
 		if (score >= 90 && score <= 100) {
 			grade = "A";
@@ -18,7 +26,12 @@ public class AddingMethods {
 		} else {
 			grade = "D";
 		}
-		System.out.println("Grade of " + studentName + " is: " + grade);
+		return grade;
 	}
 	
+	public static void displayGrades(String studentName, String grade) {
+		System.out.println("*******************************");
+		System.out.println("Grade of " + studentName + " is: " + grade);
+		System.out.println("*******************************");
+	}
 }
