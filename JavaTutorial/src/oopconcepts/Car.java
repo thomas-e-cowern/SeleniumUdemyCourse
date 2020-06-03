8ddf57529c374c45a961bece7e89611629421a77
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-03 18:38:07 -0400

    added car class

---
+++ JavaTutorial/src/oopconcepts/Car.java
@@ -0,0 +1,23 @@
+package oopconcepts;
+
+public class Car {
+
+	public static void main(String[] args) {
+
+	}
+	
+	// color
+		private String color;
+		// make
+		private String make;
+		// model
+		private String model;
+		// year
+		private int year;
+		
+
+		public void increaseSpeed() {
+			System.out.println("Increasing the speed");
+		}
+
+}
