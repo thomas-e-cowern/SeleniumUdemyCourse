bd7a6378dc4d759d9a2e3bfd5523f33e8449e14a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-03 09:29:27 -0400

    added array demo class

--- JavaTutorial/src/introduction/StringDemo.java
+++ JavaTutorial/src/introduction/StringDemo.java
@@ -6,12 +6,12 @@ public class StringDemo {
 		// String Literal - String Constant Pool
 		String str1 = "Hello";
 		
-		String str3 = "Hello";
+//		String str3 = "Hello";
 		
 		// String Object - Heap
 		String str2 = new String("Welcome");
 		
-		String str4 = new String("Welcome");
+//		String str4 = new String("Welcome");
 		
 		// Strings
 		str1 = "More Hello";
