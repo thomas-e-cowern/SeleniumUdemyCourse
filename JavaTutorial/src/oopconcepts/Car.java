117d6a435d266b22f376652d42fa2ae6dd67a35f
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-05 10:29:13 -0400

    added constructor demo

--- JavaTutorial/src/oopconcepts/Car.java
+++ JavaTutorial/src/oopconcepts/Car.java
@@ -27,4 +27,32 @@ public class Car {
 		public String getMake() {
 			return this.make;
 		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		public String getModel() {
+			return model;
+		}
+
+		public void setModel(String model) {
+			this.model = model;
+		}
+
+		public int getYear() {
+			return year;
+		}
+
+		public void setYear(int year) {
+			if (year > 1900) {
+				this.year = year;
+			} else {
+				System.out.println("This year is not valid");
+			}
+		}
 }
---
+++ JavaTutorial/src/oopconcepts/ConstructorDemo.java
@@ -0,0 +1,14 @@
+package oopconcepts;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car();
+		
+		c1.setMake("BMW");
+		System.out.println(c1.getMake());
+
+	}
+
+}
