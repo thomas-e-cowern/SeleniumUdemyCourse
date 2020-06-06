37e23b5336a3d41232e54dc7a192c00240179ad8
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-06 14:57:03 -0400

    added slenium test class

--- SeleniumWDTutorial/src/basicweb/TestClass.java
+++ SeleniumWDTutorial/src/basicweb/TestClass.java
@@ -1,10 +1,12 @@
 package basicweb;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new FirefoxDriver();
 	}
 
 }
