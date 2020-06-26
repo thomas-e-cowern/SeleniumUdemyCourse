bd0fdc2b34e5397b984cc5ef093d6474f7897184
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-26 11:16:05 -0400

    added Drag and drop

---
+++ SeleniumWDTutorial/src/actions/DragAndDrop.java
@@ -0,0 +1,43 @@
+package actions;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DragAndDrop {
+
+	WebDriver driver;
+	String baseUrl = "https://jqueryui.com/droppable/";
+	JavascriptExecutor jse;
+	
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "/Users/tcowern/Documents/SeleniumUdemyCourse/Drivers/geckodriver");
+//		System.setProperty("webdriver.chrome.driver", "/Users/tcowern/Documents/SeleniumUdemyCourse/Drivers/chromedriver");
+		driver = new FirefoxDriver();
+		jse = (JavascriptExecutor)driver;
+//		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.switchTo().frame(0);
+		driver.findElement(By.id("draggable"));
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+//		driver.close();
+		System.out.println("This test file is completed");
+	}
+}
