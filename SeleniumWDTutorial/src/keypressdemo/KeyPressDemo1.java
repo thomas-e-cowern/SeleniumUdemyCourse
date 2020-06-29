2def97a1bf0e7ada38be72f09b6b56aab4fda0cc
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-06-29 13:29:10 -0400

    updated key press demo

--- SeleniumWDTutorial/src/keypressdemo/KeyPressDemo1.java
+++ SeleniumWDTutorial/src/keypressdemo/KeyPressDemo1.java
@@ -5,14 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class KeyPressDemo1 {
 
 	WebDriver driver;
-	String baseUrl = "https://jqueryui.com/slider/";
+	String baseUrl = "https://letskodeit.teachable.com/p/practice";
 	JavascriptExecutor jse;
 	
 	@Before
@@ -29,6 +31,9 @@ public class KeyPressDemo1 {
 	@Test
 	public void test() throws InterruptedException {
 		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Thomas");
+		Thread.sleep(2000);
+		driver.findElement(By.id("alertbtn")).sendKeys(Keys.ENTER);
 	}
 
 	@After
