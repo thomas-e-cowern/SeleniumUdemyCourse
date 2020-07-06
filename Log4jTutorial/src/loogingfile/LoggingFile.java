6bf63ce28d25a71441a155721d55a88a661cb798
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-06 12:53:46 -0400

    Updated Logging file

--- Log4jTutorial/src/loogingfile/LoggingFile.java
+++ Log4jTutorial/src/loogingfile/LoggingFile.java
@@ -8,7 +8,9 @@ public class LoggingFile {
 	private static final Logger log = LogManager.getLogger(LoggingFile.class.getName());
 
 	public static void main(String[] args) {
+		log.trace("Trace message logged");
 		log.debug("Debug message logged");
+		log.info("Info message logged");
 		log.error("Error message logged");
 		log.fatal("Fatal Message Logged");
 		System.out.println("This is working");
