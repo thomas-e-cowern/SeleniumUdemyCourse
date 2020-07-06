719ac80d651300ce57f5192a1ed220a1d16d1900
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-06 10:43:41 -0400

    Added Logging console

--- Log4jTutorial/src/demo/LoggingConsole.java
+++ Log4jTutorial/src/demo/LoggingConsole.java
@@ -3,9 +3,9 @@ package demo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggingDemo {
+public class LoggingConsole {
 
-	private static final Logger log = LogManager.getLogger(LoggingDemo.class.getName());
+	private static final Logger log = LogManager.getLogger(LoggingConsole.class.getName());
 
 	public static void main(String[] args) {
 		log.debug("Debug message logged");
--- Log4jTutorial/src/loogingfile/LoggingFile.java
+++ Log4jTutorial/src/loogingfile/LoggingFile.java
@@ -1,11 +1,11 @@
-package demo;
+package loogingfile;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggingDemo {
+public class LoggingFile {
 
-	private static final Logger log = LogManager.getLogger(LoggingDemo.class.getName());
+	private static final Logger log = LogManager.getLogger(LoggingFile.class.getName());
 
 	public static void main(String[] args) {
 		log.debug("Debug message logged");
