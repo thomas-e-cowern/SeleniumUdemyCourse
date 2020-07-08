145a72f88cbbf18f2dd85a09b67c2bcc8f8d2ebb
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-08 11:37:33 -0400

    ran tests updated results 2

---
+++ TestNGTutorial/src/testclasses/TestNGBeforeClassAnnotations.java
@@ -0,0 +1,20 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class TestNGBeforeClassAnnotations {
+	@Test
+	public void f() {
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+}
---
+++ TestNGTutorial/src/testclasses/TestNGSoftAsserts.java
@@ -0,0 +1,23 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import appcode.SomeClassToTest;
+
+public class TestNGSoftAsserts {
+  
+	@Test
+	public void testSum() {
+		SoftAssert sa = new SoftAssert();
+		System.out.println("\nRunning Test -> testSum");
+		SomeClassToTest obj = new SomeClassToTest();
+		int result = obj.sumNumbers(1, 2);
+		sa.assertEquals(result, 2);
+		System.out.println("\nLine after assert 1");
+		sa.assertEquals(result, 3);
+		System.out.println("\nLine after assert 2");
+		sa.assertAll();
+	}
+	
+}
