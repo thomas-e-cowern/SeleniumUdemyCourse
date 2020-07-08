1735cbd291e4feee47162faee47dad91168d163b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-08 13:42:22 -0400

    added test ng class 1 and 2

--- TestNGTutorial/src/testclasses/TestNGBeforeClassAnnotations.java
+++ TestNGTutorial/src/testclasses/TestNGBeforeClassAnnotations.java
@@ -1,20 +1,41 @@
 package testclasses;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TestNGBeforeClassAnnotations {
-	@Test
-	public void f() {
+	
+	@BeforeClass
+	public void setUp () {
+		System.out.println("Runs once before class");
+	}
+	
+	@AfterClass
+	public void cleanUp () {
+		System.out.println("Runs once after class");
 	}
 
 	@BeforeMethod
 	public void beforeMethod() {
+		System.out.println("Running before method");
 	}
 
 	@AfterMethod
 	public void afterMethod() {
+		System.out.println("Running after method");
+	}
+	
+	@Test
+	public void testMethod1() {
+		System.out.println("Running test method 1");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("Running test method 2");
 	}
 
 }
---
+++ TestNGTutorial/src/testclasses/TestNGTestClass1.java
@@ -0,0 +1,41 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNGTestClass1 {
+  
+	@BeforeClass
+	public void setUp() {
+		System.out.println("\nTestNG_TestClass1 -> This runs once before class1");
+	}
+	
+	@AfterClass
+	public void cleanUp() {
+		System.out.println("\nTestNG_TestClass1 -> This runs once after class1");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("\nTestNG_TestClass1 -> This runs before every method");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("\nTestNG_TestClass1 -> This runs after every method");
+	}
+	
+	@Test
+	public void testMethod1() {
+		System.out.println("\nRunning Test -> TestNG_TestClass1 -> testMethod1");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("\nRunning Test -> TestNG_TestClass1 -> testMethod2");
+	}
+
+}
