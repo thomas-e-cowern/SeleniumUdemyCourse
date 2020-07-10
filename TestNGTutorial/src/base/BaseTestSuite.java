32e52ff8246a035c3a56696ee2e7eb56a64ac690
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-09 20:20:25 -0400

    updated Application tests

--- TestNGTutorial/src/base/BaseTestSuite.java
+++ TestNGTutorial/src/base/BaseTestSuite.java
@@ -1,25 +1,37 @@
 package base;
 
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTestSuite {
 
 	@BeforeClass
 	public void beforeClass() {
-		System.out.println("\nbase test suite -> before class");
+		System.out.println("base test suite -> before class");
 	}
 
 	@AfterClass
 	public void afterClass() {
-		System.out.println("\nbase test suite -> after class");
+		System.out.println("base test suite -> after class");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("base test suite -> before test");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("base test suite -> after test");
 	}
 
 	@BeforeSuite
 	public void beforeSuite() {
-		System.out.println("\nbase test suite -> before suite");
+		System.out.println("base test suite -> before suite");
 	}
 
 	@AfterSuite
