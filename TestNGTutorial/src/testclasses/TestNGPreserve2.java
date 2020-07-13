2c868b532289e14c66e947d366fbd7f1d98fb0c8
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-13 16:47:04 -0400

    updated  enable timeout

---
+++ TestNGTutorial/src/testclasses/TestNGPreserve2.java
@@ -0,0 +1,17 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+
+public class TestNGPreserve2 {
+  
+	@Test
+	public void testMethod1() {
+		System.out.println("TestNG_Preserve2 -> testMethod1");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("TestNG_Preserve2 -> testMethod2");
+	}
+	
+}
