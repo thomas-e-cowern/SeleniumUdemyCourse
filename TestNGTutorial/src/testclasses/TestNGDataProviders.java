5b2dae24ba270a5819e281daccdf18efd27f2283
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-16 08:09:30 -0400

    updated data providers

--- TestNGTutorial/src/testclasses/TestNGDataProviders.java
+++ TestNGTutorial/src/testclasses/TestNGDataProviders.java
@@ -1,9 +1,23 @@
 package testclasses;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNGDataProviders {
-  @Test
-  public void f() {
-  }
+  
+	@DataProvider(name="inputs")
+	public Object[][] getData() {
+		return new Object[][] {
+			{"bmw", "m3"},
+			{"audi", "a6"},
+			{"benz", "c300"}
+		};
+	}
+	
+	@Test(dataProvider="inputs")
+	public void testMethod1(String input1, String input2) {
+		System.out.println("Input 1: " + input1);
+		System.out.println("Input 2: " + input2);
+	}
+	
 }
