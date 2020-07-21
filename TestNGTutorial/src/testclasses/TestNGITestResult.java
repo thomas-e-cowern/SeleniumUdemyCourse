28aada259d7201de0db39319213c9391b5c422ad
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-21 15:19:27 -0400

    updated test result

--- TestNGTutorial/src/testclasses/TestNGITestResult.java
+++ TestNGTutorial/src/testclasses/TestNGITestResult.java
@@ -1,14 +1,27 @@
 package testclasses;
 
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
 public class TestNGITestResult {
+	
   @Test
-  public void f() {
+  public void testMethod1() {
+	  System.out.println("Running --> Test Method One");
+	  Assert.assertTrue(false);
   }
+  
+  @Test
+  public void testMethod2() {
+	  System.out.println("Running --> Test Method Two");
+	  Assert.assertTrue(true);
+  }
+  
   @AfterMethod
-  public void afterMethod() {
+  public void afterMethod(ITestResult result) {
+	  System.out.println(result.getMethod().getMethodName());
   }
 
 }
