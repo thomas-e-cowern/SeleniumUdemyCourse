dd3dbe9e752a44d004f1eec000973eff05cbe54b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-22 09:56:58 -0400

    Added methods to screenshot test

--- TestNGTutorial/src/testclasses/TestNGScreenshotTest.java
+++ TestNGTutorial/src/testclasses/TestNGScreenshotTest.java
@@ -1,19 +1,25 @@
 package testclasses;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TestNGScreenshotTest {
-  @Test
-  public void f() {
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
-
-  @AfterMethod
-  public void afterMethod() {
-  }
+	
+	WebDriver driver;
+	String baseUrl;
+	
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+	
+	@Test
+	public void testOne() {
+	}
+	
+	@AfterMethod	
+	public void afterMethod() {
+	}
 
 }
