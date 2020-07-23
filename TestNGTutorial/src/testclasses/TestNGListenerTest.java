163c2a7a0d67d1bd55941575faba73589c6fe970
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-23 13:40:59 -0400

    added listener test methods

--- TestNGTutorial/src/testclasses/TestNGListenerTest.java
+++ TestNGTutorial/src/testclasses/TestNGListenerTest.java
@@ -2,24 +2,31 @@ package testclasses;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class TestNGListenerTest {
   
   @BeforeClass
   public void beforeClass() {
+	  System.out.println("This is the code in beforeClass");
   }
 
   @Test
   public void test1() {
+	  System.out.println("This is the code in test1");
+	  Assert.assertTrue(true);
   }
   
   @Test
   public void test2() {
+	  System.out.println("This is the code in test2");
+	  Assert.assertTrue(false);
   }
   
   @AfterClass
   public void afterClass() {
+	  System.out.println("This is the code in afterClass");
   }
 
 }
