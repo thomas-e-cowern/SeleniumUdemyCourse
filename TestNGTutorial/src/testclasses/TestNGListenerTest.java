34a09bc22a28b1b5acaa8e5c74574a7d408dbf68
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-23 13:37:56 -0400

    added listener test

---
+++ TestNGTutorial/src/testclasses/TestNGListenerTest.java
@@ -0,0 +1,25 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNGListenerTest {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @Test
+  public void test1() {
+  }
+  
+  @Test
+  public void test2() {
+  }
+  
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
