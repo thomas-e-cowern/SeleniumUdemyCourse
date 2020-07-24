e5dc705d65960e0506c4ec502be18d523f2f26ff
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-24 17:58:00 -0400

    updated custom listeners class

--- TestNGTutorial/src/listeners/CustomListeners.java
+++ TestNGTutorial/src/listeners/CustomListeners.java
@@ -1,5 +1,97 @@
 package listeners;
 
-public class CustomListeners {
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
 
+public class CustomListeners implements IInvokedMethodListener, ITestListener, ISuiteListener {
+	
+
+	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+		//Before every method in the test class
+		System.out.println("before invocation: " + testResult.getTestClass().getName() + " => " + method.getTestMethod().getMethodName());
+	}
+	
+	public void afterInvocation(IInvokedMethod method, ITestResult testResults) {
+		//After every method in the test class
+		System.out.println("after invocation: " + method);
+	}
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("onTestStart => Test name: " + result.getName());
+		ITestListener.super.onTestStart(result);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("onTestSuccess => Test name: " + result.getName());
+		ITestListener.super.onTestSuccess(result);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("onTestFailure => Test name: " + result.getName());
+		ITestListener.super.onTestFailure(result);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("onTestSkipped => Test name: " + result.getName());
+		ITestListener.super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("onStart => Test name: " + context.getName());
+		ITestNGMethod methods[] = context.getAllTestMethods();
+		System.out.println("These methods will be executed in this test tag");
+		for (ITestNGMethod method : methods) {
+			System.out.println(method.getMethodName());
+		}
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("onFinish => Test name: " + context.getName());
+		ITestListener.super.onFinish(context);
+	}
+	
+	@Override
+	public void onStart(ISuite suite) {
+		// <suite> tag starts
+		System.out.println("Coming from suite onStart");
+		ISuiteListener.super.onStart(suite);
+	}
+
+	@Override
+	public void onFinish(ISuite suite) {
+		// <suite> tag ends
+		System.out.println("Coming from suite onFinish");
+		ISuiteListener.super.onFinish(suite);
+	}
 }
