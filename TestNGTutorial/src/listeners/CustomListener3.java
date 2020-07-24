6d6770db4234148868089b4ec949ad200d2bbd29
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-24 14:21:13 -0400

    Added custom listener 3

---
+++ TestNGTutorial/src/listeners/CustomListener3.java
@@ -0,0 +1,28 @@
+package listeners;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+public class CustomListener3 implements ISuiteListener {
+
+	@Override
+	public void onStart(ISuite suite) {
+		// <suite> tag starts
+		System.out.println("Coming from suite onStart");
+		ISuiteListener.super.onStart(suite);
+	}
+
+	@Override
+	public void onFinish(ISuite suite) {
+		// <suite> tag ends
+		System.out.println("Coming from suite onFinish");
+		ISuiteListener.super.onFinish(suite);
+	}
+
+	
+	
+}
