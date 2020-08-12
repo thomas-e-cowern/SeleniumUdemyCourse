c7623594402970cef7488066b50e36e1b2d382df
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-08-12 17:48:53 -0400

    added single dataset utility

--- SeleniumWDTutorial/src/Data/Constants.java
+++ SeleniumWDTutorial/src/Data/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 	public static final String URL = "http://www.letskodeit.com/";
 	public static final String File_Path = "//Users/tcowern/Documents/SeleniumUdemyCourse/Data/";
 	public static final String File_Name = "ExcelData.xlsx";
+	public static final String File_Name_Write = "ExcelDataWrite.xlsx";
 }
