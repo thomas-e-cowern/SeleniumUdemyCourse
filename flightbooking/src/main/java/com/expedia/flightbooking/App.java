845a5ca319ab1e55dfbb24a8e2b17581142b2b87
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-15 14:59:40 -0400

    maven project flightbooking initial commit

---
+++ flightbooking/src/main/java/com/expedia/flightbooking/App.java
@@ -0,0 +1,13 @@
+package com.expedia.flightbooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
